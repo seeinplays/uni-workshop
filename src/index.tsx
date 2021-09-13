1384b9c4a46a34a25f364ee062d272347c5530da
dOrgJelli <jelli@dorg.tech>
2021-09-13 12:37:52 -0400

    init wallet integration

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createWeb3ReactRoot, Web3ReactProvider } from "@web3-react/core";
+import { Web3Provider } from '@ethersproject/providers'
+
+function getLibrary(provider: any): Web3Provider {
+  const library = new Web3Provider(provider, 'any')
+  library.pollingInterval = 15000
+  return library
+}
+
+const NetworkContextName = "NETWORK";
+
+const Web3RootProvider = createWeb3ReactRoot(NetworkContextName);
+
+if (!!(window as any).ethereum) {
+  (window as any).ethereum.autoRefreshOnNetworkChange = false;
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Web3RootProvider getLibrary={getLibrary}>
+        <App />
+      </Web3RootProvider>
+    </Web3ReactProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
